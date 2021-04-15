fda4efbcf2ab5e15da70da8b4d8143c2dffb32af
ValeriaTishina <lyolik2038@gmail.com>
2021-04-15 20:28:51 +0400

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Валерия Тишина on 15.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
